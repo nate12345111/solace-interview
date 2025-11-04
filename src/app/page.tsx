33c6fbfec8986c0e17eb40d96489097f0c274494
Nathan-Patnam <nathanpatnam@hotmail.com>
2025-11-05 01:29:53 +0800

    Ensure reset button actually resets search query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const [advocates, setAdvocates] = useState([]);
   const [filteredAdvocates, setFilteredAdvocates] = useState([]);
+  const [currentSearchTerm, setCurrentSearchTerm] = useState("");
 
   useEffect(() => {
     console.log("fetching advocates...");
@@ -19,9 +20,8 @@ export default function Home() {
   const onChange = (e) => {
     const searchTerm = e.target.value;
 
-    document.getElementById("search-term").innerHTML = searchTerm;
 
-    console.log("filtering advocates...");
+    setCurrentSearchTerm(searchTerm);
     const filteredAdvocates = advocates.filter((advocate) => {
       return (
         advocate.firstName.includes(searchTerm) ||
@@ -37,7 +37,7 @@ export default function Home() {
   };
 
   const onClick = () => {
-    console.log(advocates);
+    setCurrentSearchTerm("")
     setFilteredAdvocates(advocates);
   };
 
@@ -51,20 +51,22 @@ export default function Home() {
         <p>
           Searching for: <span id="search-term"></span>
         </p>
-        <input style={{ border: "1px solid black" }} onChange={onChange} />
+        <input style={{ border: "1px solid black" }} onChange={onChange} value={currentSearchTerm} />
         <button onClick={onClick}>Reset Search</button>
       </div>
       <br />
       <br />
       <table>
         <thead>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>City</th>
-          <th>Degree</th>
-          <th>Specialties</th>
-          <th>Years of Experience</th>
-          <th>Phone Number</th>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>City</th>
+            <th>Degree</th>
+            <th>Specialties</th>
+            <th>Years of Experience</th>
+            <th>Phone Number</th>
+          </tr>
         </thead>
         <tbody>
           {filteredAdvocates.map((advocate) => {
